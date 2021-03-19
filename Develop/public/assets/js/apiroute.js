d8b7704bc9a140e6f50e831f4d89c0b7aec5d7ef
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-03-18 19:31:53 -0600

    started working on api route js created consts

---
+++ Develop/public/assets/js/apiroute.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+
+let noteContents; 
+
