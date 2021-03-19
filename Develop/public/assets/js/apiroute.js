5f79d3850d462fb3dd4f5d338b3587e49b08b4d1
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-03-18 19:51:16 -0600

    created a post route in the apiroute.js file

--- Develop/public/assets/js/apiroute.js
+++ Develop/public/assets/js/apiroute.js
@@ -13,3 +13,20 @@ app.get ("/api/notes", function (res,req) {
     })
 
 })
+
+// Recieves notes to save, add the notes to the db.json, and then returns the notes to the client
+app.post ("/api/notes", function (res,req) {
+    let newNote = req.body;
+    let lastId = 0;
+    if (noteContents.length !== 0) {
+        lastId = noteContents[noteContents.length - 1]["id"];
+    }
+    let newId = lastId + 1;
+    newNote["id"] = newId;
+    noteContents.push(newNote);
+    writeFileAsync("db/db.json", JSON.stringify(noteContents)).then(function () {
+        console.log("Note has been updated");
+    });
+    res.json(newNote);
+});
+
