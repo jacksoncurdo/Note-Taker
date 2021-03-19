408f3bbaab90d3f0edaf3e279fc771b4c4031cff
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-03-18 19:41:55 -0600

    added app.get for the route to the api in apiroute.js

--- Develop/public/assets/js/apiroute.js
+++ Develop/public/assets/js/apiroute.js
@@ -5,3 +5,11 @@ const readFileAsync = util.promisify(fs.readFile);
 
 let noteContents; 
 
+// Reads the db.json file and returns all saved notes as JSON
+app.get ("/api/notes", function (res,req) {
+    readFileAsync("db/db.json", "utf8").then(function (data) {
+        noteContents = JSON.parse(data)
+        res.json(noteContents);
+    })
+
+})
