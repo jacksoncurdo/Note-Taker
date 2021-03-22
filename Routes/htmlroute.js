d2887440db483cdd6e22b601841d2ec4254b7b92
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-03-21 20:19:04 -0600

    wrote the code for the html route, made routes to html pages using the GET method

--- Routes/htmlroute.js
+++ Routes/htmlroute.js
@@ -1,3 +1,15 @@
 let path = require("path");
 
-const router = require('express').Router(); 
+// Using the GET method we are sending the user a html page 
+// GET is an Express method, one of the methods included in CRUD
+module.exports = (app) => {
+
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  // If there is not a matching route then the default will be home
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
