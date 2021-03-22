b9aab4e082ecdb0d6a352e260c4652312699f26e
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-03-21 20:54:02 -0600

    finished writing the code for server.js

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ const app = express();
 // I don't really understand the difference between port numbers but I have seen 8080 used before so I went with that.
 const PORT = process.env.PORT || 8080;
 
+// Returning json using express 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Using express to deliever static files 
+app.use(express.static("public"));
+
+// Requiring the files 
+require ('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+// Telling the server to listen 
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
