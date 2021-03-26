b33279d2210142943bd37fe4a7536857df432e3f
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-03-26 16:39:25 -0600

    fixed all bugs and syntax errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const fs = require("fs");
 const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -8,45 +9,24 @@ module.exports = function (app) {
 
 
 // Reads the db.json file and returns all saved notes as JSON
-app.get ("/api/notes", function (res,req) {
+app.get("/api/notes", function (req, res) {
     readFileAsync("db/db.json", "utf8").then(function (data) {
         noteContents = JSON.parse(data)
         res.json(noteContents);
     })
-
-});
-
+})
 // Recieves notes to save, add the notes to the db.json, and then returns the notes to the client
-app.post ("/api/notes", function (res,req) {
-    let newNote = req.body;
-    let lastId = 0;
-    if (noteContents.length !== 0) {
-        lastId = noteContents[noteContents.length - 1]["id"];
-    }
-    let newId = lastId + 1;
-    newNote["id"] = newId;
-    noteContents.push(newNote);
-    writeFileAsync("db/db.json", JSON.stringify(noteContents)).then(function () {
-        console.log("Note has been updated");
-    });
-    res.json(newNote);
+app.post("/api/notes", (req,res)=>{
+    fs.readFile("db/db.json", "utf8", (err,data)=>{
+        if (err) throw err;
+let savedNotes = JSON.parse(data);
+let updatedNotes = [req.body, ...savedNotes];
+fs.writeFile("db/db.json", JSON.stringify(updatedNotes), err => {
+    if (err) throw err;
+    res.status(200);
 });
+    }) 
+ }) 
 
-// Deletes notes
-app.delete ("/api/notes", function (res,req) {
-    let chosenId = parseInt(req.params.id);
-    for (let i = 0; i < noteContents.length; i++) {
-        if (chosenId === noteContents[i].id) {
-            noteContents.splice(i, 1);
-            let noteJSON = JSON.stringify(noteContents, null, 2);
-            writeFileAsync("/db.json", noteJSON).then(function () {
-                console.log("Note deleted");
-            });
-        }
-    }
-    res.json(noteContents);
-});
 }
-
-
 
